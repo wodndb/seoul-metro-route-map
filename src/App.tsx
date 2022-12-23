70272066373aa4074ae97844691daca15e93392c
Jaeu Jeong <wodndb@gmail.com>
2022-12-23 23:03:20 +0900

    [add] konvajs and test it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layer, Stage, Text } from "react-konva";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Text x={20} y={20} text={"Hello World"} />
+      </Layer>
+    </Stage>
   );
 }
 
